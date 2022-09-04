23ecf24f104638e107d2532ecf3babfa8a4e6d27
JuinEsp <franciscoanto@gmail.com>
2022-09-03 23:28:00 -0300

    Iniciado componente Header

--- resources/js/Pages/Site.jsx
+++ resources/js/Pages/Site.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Head } from '@inertiajs/inertia-react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 
 export default function Site(props) {
@@ -10,30 +10,75 @@ export default function Site(props) {
             <header className='header-default'>
                 <div id="demo_loc"></div>
                 <Container>
-                    <div className="navbar-header">
-                        <div className="header-details">
-                            <div className="header-item header-phone left">
-                                <table>
-                                    <tbody><tr>
-                                        <td><i className="fa fa-phone"></i></td>
-                                        <td className="header-item-text">
-                                            <a href="tel:(85) 3461-1166">
-                                                Ligue<br />
+                    <Row className="show-grid">
+                        <Col xs={12} md={4}>
+                            <a href="#" className="navbar-brand">
+                                <img src="https://i.imgur.com/3aULwmb.png" alt="Espindola imobiliária" />
+                            </a>
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <div className="header-details">
+                                <Col md={4}>
+                                <div className="header-item header-phone left">
+                                    <table>
+                                        <tbody><tr>
+                                            <td><i className="fa fa-phone"></i></td>
+                                            <td className="header-item-text">
+                                                <a href="tel:(85) 3461-1166">
+                                                    Ligue<br />
+                                                    <span>
+                                                        (85) 3461-1166
+                                                    </span></a>
 
-                                                <span>
-                                                    (85) 3461-1166
-                                                </span></a>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                </Col>
+                                <Col md={4}>
+                                <div className="header-item header-phone left">
+                                    <table>
+                                        <tbody><tr>
+                                            <td><i className="fa fa-phone"></i></td>
+                                            <td className="header-item-text">
+                                                <a href="https://api.whatsapp.com/send?phone=5585988101166&text=Ol%C3%A1,%20eu%20estou%20com%20uma%20d%C3%BAvida,%20poderia%20me%20ajudar?">
+                                                    Whatsapp<br />
+                                                    <span>
+                                                    (85) 98810-1166
+                                                    </span></a>
 
-                                        </td>
-                                    </tr>
-                                    </tbody></table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                </Col>
+                                <Col md={4}>
+                                <div className="header-item header-phone left">
+                                    <table>
+                                        <tbody><tr>
+                                            <td><i className="fa fa-phone"></i></td>
+                                            <td className="header-item-text">
+                                                <span>E-mail</span><br />
+                                            <a href="mailto: meajuda@espindolaimobiliaria.com.br">meajuda@espindolaimobiliaria.com.br</a>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                </Col>
                             </div>
-                            <div className="clear"></div>
-                        </div>
-                    </div>
-                    <a href="#" className="navbar-brand">
-                        <img src="https://i.imgur.com/3aULwmb.png" alt="Espindola imobiliária" />
-                    </a>
+                           
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+                        </Col>
+                    </Row>
+
+
                 </Container>
             </header>
         </>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: [
+                'resources/js/bootstrap.min.js',
+                'resources/js/app.jsx'
+            ],
             refresh: true,
         }),
         react(),
