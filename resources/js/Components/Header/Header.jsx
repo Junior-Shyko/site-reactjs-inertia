1d2b0a4a031ff8588f22a225485babfdfad8f705
JuinEsp <franciscoanto@gmail.com>
2022-09-10 01:03:31 -0300

    Up no layout md

--- resources/js/Components/Header/Header.jsx
+++ resources/js/Components/Header/Header.jsx
@@ -1,130 +1,123 @@
-import React, { useEffect } from "react"
-import {Grid, Row, Col, Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+import React, { useEffect, useState } from "react"
+import { Grid, Row, Col, Navbar, Nav, NavItem, NavDropdown, MenuItem, Table } from 'react-bootstrap'
 
 
 export default Header => {
-
+    const [contact, setContact] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:50000/api/contact')
-            .then(response => {
-                console.log({ response })
+        fetch('http://localhost:5050/api/contact')
+            .then((response) => response.json())
+            .then((actualData) => {
+                setContact(actualData)
             })
             .catch(err => {
                 console.log({ err });
             })
+
     }, [])
 
-
-
     return (
-        <header className='header-default' style={{background: 'white'}}>
+        <header className="header-default">
             <div id="demo_loc"></div>
-            <style type="text/css">
-            {`
-            .nav-custom {
-                background-color: transparent !important;
-                border-color: transparent !important;
-            }
-            `}
-            </style>
             <Grid>
-                <Row className="show-grid">
-                    <Col xs={12} md={4}>
-                        <a href="#" className="navbar-brand">
-                            <img src="https://i.imgur.com/3aULwmb.png" alt="Espindola imobiliária" />
-                        </a>
-                    </Col>
-                    <Col xs={12} md={8}>
-                    <div className="header-details">
-                        <Col md={4}>
-                            <div className="header-item header-phone left">
-                                <table>
-                                    <tbody><tr>
-                                        <td><i className="fa fa-phone"></i></td>
-                                        <td className="header-item-text">
-                                            <a href="tel:(85) 3461-1166">
-                                                Ligue<br />
-                                                <span>
-                                                    (85) 3461-1166
-                                                </span></a>
+                <Navbar collapseOnSelect>
+                    <Navbar.Header>
+                        <div className="header-details">
+                            <Col xs={12} md={12}>
+                                <Col md={8} style={{ float: 'right' }}>
+                                    <Col md={4}>
+                                        <div className="header-item header-phone left">
+                                            <Table>
+                                                <thead>
+                                                    <tr>
+                                                        <td><i className="fa fa-phone"></i></td>
+                                                        <td className="header-item-text">
+                                                            <a href="tel:(85) 3461-1166">
+                                                                Ligue<br />
+                                                                <span>{contact.phoneFixed}</span>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                </thead>
+                                            </Table>
+                                        </div>
+                                    </Col>
+                                    <Col md={4}>
+                                        <div className="header-item header-phone left">
+                                            <Table>
+                                                <thead>
+                                                    <tr>
+                                                        <td><i className="fa fa-whatsapp"></i></td>
+                                                        <td className="header-item-text">
+                                                            <a href="https://api.whatsapp.com/send?phone=5585988101166&text=Ol%C3%A1,%20eu%20estou%20com%20uma%20d%C3%BAvida,%20poderia%20me%20ajudar?">
+                                                                WhatsApp<br />
+                                                                <span>{contact.phoneMobile}</span>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                </thead>
+                                            </Table>
+                                        </div>
+                                    </Col>
+                                    <Col md={4}>
+                                        <div className="header-item header-phone left">
+                                            <Table>
+                                                <thead>
+                                                    <tr>
+                                                        <td><i className="fa fa-envelope"></i></td>
+                                                        <td className="header-item-text">
+                                                            E-mail<br />
+                                                            <span>
+                                                                <a href="mailto: `{contact.email}`">
+                                                                    {contact.email}
+                                                                </a>
 
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </Col>
-                        <Col md={4}>
-                            <div className="header-item header-phone left">
-                                <table>
-                                    <tbody><tr>
-                                        <td><i className="fa fa-whatsapp"></i></td>
-                                        <td className="header-item-text">
-                                            <a href="https://api.whatsapp.com/send?phone=5585988101166&text=Ol%C3%A1,%20eu%20estou%20com%20uma%20d%C3%BAvida,%20poderia%20me%20ajudar?">
-                                                Whatsapp<br />
-                                                <span>
-                                                    (85) 98810-1166
-                                                </span></a>
+                                                            </span>
+                                                        </td>
+                                                    </tr>
+                                                </thead>
+                                            </Table>
+                                        </div>
+                                    </Col>
+                                </Col>
+                                <Col md={4} style={{ float: 'left' }}>
+                                    <Navbar.Brand>
+                                        <a href="#" className="navbar-brand">
+                                            <img src="https://i.imgur.com/3aULwmb.png" alt="Espindola imobiliária" />
+                                        </a>
+                                    </Navbar.Brand>
+                                </Col>
+                            </Col>
+                        </div>
 
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </Col>
-                        <Col md={4}>
-                            <div className="header-item header-phone left">
-                                <table>
-                                    <tbody><tr>
-                                        <td><i className="fa fa-envelope"></i></td>
-                                        <td className="header-item-text">
-                                            <span>E-mail</span><br />
-                                            <a href="mailto: meajuda@espindolaimobiliaria.com.br">meajuda@espindolaimobiliaria.com.br</a>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </Col>
-                    </div>
-                </Col>
-                </Row>
-                <Row>
-                    <Navbar collapseOnSelect className="nav-custom">
-                    <Navbar.Header >
-                        <Navbar.Brand >
-          
-                        </Navbar.Brand>
-                        <Navbar.Toggle/>
+                        <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse className="main-menu-wrap">
                         <Nav>
-                        <NavItem eventKey={1} href="#">
-                            Link
-                        </NavItem>
-                        <NavItem eventKey={2} href="#">
-                            Link
-                        </NavItem>
-                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}>Action</MenuItem>
-                            <MenuItem eventKey={3.2}>Another action</MenuItem>
-                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                            <MenuItem divider />
-                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                        </NavDropdown>
+                            <NavItem eventKey={1} href="#">
+                                Link
+                            </NavItem>
+                            <NavItem eventKey={2} href="#">
+                                Link
+                            </NavItem>
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                            </NavDropdown>
                         </Nav>
                         <Nav pullRight>
-                        <NavItem eventKey={1} href="#">
-                            Link Right
-                        </NavItem>
-                        <NavItem eventKey={2} href="#">
-                            Link Right
-                        </NavItem>
+                            <NavItem eventKey={1} href="#">
+                                Link Right
+                            </NavItem>
+                            <NavItem eventKey={2} href="#">
+                                Link Right
+                            </NavItem>
                         </Nav>
                     </Navbar.Collapse>
-                    </Navbar>
-
-                </Row>
+                </Navbar>
             </Grid>
         </header>
     )
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
